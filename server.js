eb0864ae1903698f892963dc648e942b1f6e0a7b
Annevd <144004647+Annevd@users.noreply.github.com>
2024-03-07 10:46:28 +0100

    V5
    nieuwe get routes gemaakt

--- server.js
+++ server.js
@@ -32,34 +32,57 @@ app.use(express.urlencoded({ extended: true }));
 
 // 2. Routes die HTTP Request and Responses afhandelen
 
-// Haal alle squads uit de WHOIS API op
-const storyData = await fetchJson(apiUrl + '/tm_story')
-const languageData = await fetchJson(apiUrl + '/tm_language')
-const speakerData = await fetchJson(apiUrl + '/tm_speaker_profile')
-const audioData = await fetchJson(apiUrl + '/tm_audio')
-const playlistData = await fetchJson(apiUrl + '/tm_playlist')
-
-
 // Maak een GET route voor de index
-// Stap 1
+
 app.get('/', function (request, response) {
-  // Stap 2
-  // Haal alle personen uit de WHOIS API op
+
+  Promise.all([ // Fetch data from all endpoints concurrently using Promise.all()
+    fetchJson(apiUrl + '/tm_story'), // Fetch data from the tm_story endpoint
+    fetchJson(apiUrl + '/tm_language'), // Fetch data from the tm_language endpoint
+    fetchJson(apiUrl + '/tm_playlist'), // Fetch data from the tm_playlist endpoint
+    fetchJson(apiUrl + '/tm_audio') // Fetch data from the tm_audio endpoint
+  ]).then(([storyData, languageData, playlistData, audioData]) => {
+// After all promises are resolved, this function will be executed with the fetched data
+
+// Render the 'index.ejs' template and pass all fetched data to the view   
+ response.render('index', {
+      stories: storyData.data, // Pass fetched story data to the view under the 'stories' key
+      languages: languageData.data, // Pass fetched language data to the view under the 'languages' key
+      playlists: playlistData.data, // Pass fetched playlist data to the view under the 'playlists' key
+      audio: audioData.data}) // Pass fetched audio data to the view under the 'audio' key
+    }); 
+  })
+
+// Maak een GET route voor de lessons pagina
+
+app.get('/lessons', function (request, response) {
+
   Promise.all([
     fetchJson(apiUrl + '/tm_story'),
     fetchJson(apiUrl + '/tm_language'),
     fetchJson(apiUrl + '/tm_playlist'),
     fetchJson(apiUrl + '/tm_audio')
   ]).then(([storyData, languageData, playlistData, audioData]) => {
-    // apiData bevat gegevens van alle personen uit alle squads
-    // Je zou dat hier kunnen filteren, sorteren, of zelfs aanpassen, voordat je het doorgeeft aan de view
-    // Stap 3
-    // Render index.ejs uit de views map en geef de opgehaalde data mee als variabele, genaamd persons
+    
+    response.render('lessons', {
+      stories: storyData.data,
+      languages: languageData.data,
+      playlists: playlistData.data,
+      audio: audioData.data})
+  })
+})
 
-    // Stap 4
-    // HTML maken op basis van JSON data
-    response.render('index', {
-      // persons: persons.data,
+// Maak een GET route voor de stories pagina
+
+app.get('/stories', function(request, response) {
+  Promise.all([
+    fetchJson(apiUrl + '/tm_story'),
+    fetchJson(apiUrl + '/tm_language'),
+    fetchJson(apiUrl + '/tm_playlist'),
+    fetchJson(apiUrl + '/tm_audio')
+  ]).then(([storyData, languageData, playlistData, audioData]) => {
+    
+    response.render('stories', {
       stories: storyData.data,
       languages: languageData.data,
       playlists: playlistData.data,
