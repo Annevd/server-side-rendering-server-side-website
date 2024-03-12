c87181661bfc7727572b83a8b9684371aeda3257
Annevd <144004647+Annevd@users.noreply.github.com>
2024-03-12 13:31:15 +0100

    V9
    refactored

--- public/script.js
+++ public/script.js
@@ -1,3 +1,5 @@
+document.addEventListener('DOMContentLoaded', function () {
+
 const openMenuButton = document.querySelector(".menu-button.home")
 const closeMenuButton = document.querySelector(".menu-button.menu")
 const navShown = document.querySelector("nav")
@@ -5,15 +7,15 @@ const navShown = document.querySelector("nav")
 // Opent het menu
 openMenuButton.addEventListener('click', function() {
     navShown.classList.add("menu-open") // opent het menu
-    openMenuButton.classList.add("hidden") // verbergt het open menu knopje
+    openMenuButton.classList.add("hidden-menu") // verbergt het open menu knopje
     document.querySelector(".menu-button.menu").style.display = "flex";
 })
 
 // Sluit het menu
 closeMenuButton.addEventListener('click', function() {
     navShown.classList.remove("menu-open") // sluit het menu
-    openMenuButton.classList.remove("hidden") // laat het open menu knopje weer zien
+    openMenuButton.classList.remove("hidden-menu") // laat het open menu knopje weer zien
     document.querySelector(".menu-button.menu").style.display = "none";
 })
 
-
+})
--- server.js
+++ server.js
@@ -35,6 +35,18 @@ app.use(express.urlencoded({ extended: true }));
 // Maak een GET route voor de index
 
 app.get('/', function (request, response) {
+    response.render('index')
+})
+
+// Maak een GET route voor de testing pagina
+
+app.get('/testing', function (request, response) {
+  response.render('testing')
+})
+
+// Maak een GET route voor de lessons pagina
+
+app.get('/lessons', function (request, response) {
 
   Promise.all([ // Fetch data from all endpoints concurrently using Promise.all()
     fetchJson(apiUrl + '/tm_story'), // Fetch data from the tm_story endpoint
@@ -45,36 +57,17 @@ app.get('/', function (request, response) {
 // After all promises are resolved, this function will be executed with the fetched data
 
 // Render the 'index.ejs' template and pass all fetched data to the view   
- response.render('index', {
+ response.render('lessons', {
       stories: storyData.data, // Pass fetched story data to the view under the 'stories' key
       languages: languageData.data, // Pass fetched language data to the view under the 'languages' key
       playlists: playlistData.data, // Pass fetched playlist data to the view under the 'playlists' key
       audio: audioData.data}) // Pass fetched audio data to the view under the 'audio' key
-    }); 
-  })
-
-// Maak een GET route voor de lessons pagina
-
-app.get('/lessons', function (request, response) {
-
-  Promise.all([
-    fetchJson(apiUrl + '/tm_story'),
-    fetchJson(apiUrl + '/tm_language'),
-    fetchJson(apiUrl + '/tm_playlist'),
-    fetchJson(apiUrl + '/tm_audio')
-  ]).then(([storyData, languageData, playlistData, audioData]) => {
-    
-    response.render('lessons', {
-      stories: storyData.data,
-      languages: languageData.data,
-      playlists: playlistData.data,
-      audio: audioData.data})
-  })
+    });
 })
 
 // Maak een GET route voor de stories pagina
 
-app.get('/stories', function(request, response) {
+app.get('/lessons/stories', function(request, response) {
   Promise.all([
     fetchJson(apiUrl + '/tm_story'),
     fetchJson(apiUrl + '/tm_language'),
@@ -88,6 +81,18 @@ app.get('/stories', function(request, response) {
       playlists: playlistData.data,
       audio: audioData.data})
   })
+})
+
+// Maak een GET route voor de statistics pagina
+
+app.get('/statistics', function (request, response) {
+  response.render('statistics')
+})
+
+// Maak een GET route voor de profile pagina
+
+app.get('/profile', function (request, response) {
+  response.render('profile')
 })
 
 // 3. Start de webserver
