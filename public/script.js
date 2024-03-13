2d088de9d2194d615b0d1741aee7b4acd230bdd4
Annevd <144004647+Annevd@users.noreply.github.com>
2024-03-13 13:33:36 +0100

    V10
    latest version with old comments

--- public/script.js
+++ public/script.js
@@ -6,6 +6,7 @@ const navShown = document.querySelector("nav")
 
 // Opent het menu
 openMenuButton.addEventListener('click', function() {
+    document.documentElement.classList.add("no-scroll") // zorgt dat je de body niet kan scrollen als je in het menu bent
     navShown.classList.add("menu-open") // opent het menu
     openMenuButton.classList.add("hidden-menu") // verbergt het open menu knopje
     document.querySelector(".menu-button.menu").style.display = "flex";
@@ -13,6 +14,7 @@ openMenuButton.addEventListener('click', function() {
 
 // Sluit het menu
 closeMenuButton.addEventListener('click', function() {
+    document.documentElement.classList.remove("no-scroll")
     navShown.classList.remove("menu-open") // sluit het menu
     openMenuButton.classList.remove("hidden-menu") // laat het open menu knopje weer zien
     document.querySelector(".menu-button.menu").style.display = "none";
